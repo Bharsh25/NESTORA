d1e598679e8459392b8cb9861506ac27be3882f7
Harsh Bhendarkar <bhendarkarharsh92@gmail.com>
2025-11-26 16:57:58 +0530

    fix

--- controllers/listing.js
+++ controllers/listing.js
@@ -7,7 +7,7 @@ const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 
 module.exports.index=async (req, res) => {
-    const allListings = await Listing.find();
+    const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
     };
 
